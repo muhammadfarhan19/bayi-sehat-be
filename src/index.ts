26417fec8440d060f0d7b656447a14bf1c6c8483
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-05-26 15:41:34 +0700

    change route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express, { Application } from 'express'
-import { routes } from './routes'
 import { logger } from './utils/logger'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import deserializeToken from './middleware/deserializedToken'
-import { BabyRouter } from './routes/baby.route'
-import { AuthRouter } from './routes/auth.route'
-import { BabyConditionRouter } from './routes/condition.route'
+import { createUserSession, getUsers, refreshSession, userRegistration } from './controllers/auth.controller'
+import {
+  createBaby,
+  deleteBaby,
+  getBaby,
+  getBabyDetail,
+  getFemaleBaby,
+  getMaleBaby,
+  updateBaby
+} from './controllers/baby.controller'
+import { requireBaby } from './middleware/baby'
+import {
+  DeleteBabyCondition,
+  createBabyCondition,
+  getBabyConditions,
+  getDetailBabyCondition,
+  updateBabyCondition
+} from './controllers/condition.controller'
 
 const app: Application = express()
 const port: number = 4000
@@ -43,6 +57,25 @@ app.use(deserializeToken)
 app.get('/', (req, res) => {
   res.send('Express on Vercel')
 })
+
+app.get('/auth/', getUsers)
+app.post('/auth/register', userRegistration)
+app.post('/auth/login', createUserSession)
+app.post('/auth/refresh', refreshSession)
+
+app.get('/baby/', getBaby)
+app.get('/baby/detail/:id', getBabyDetail)
+app.post('/baby/create', createBaby)
+app.delete('/baby/delete/:id', deleteBaby)
+app.put('/baby/put/:id', updateBaby)
+app.get('/baby/male', getMaleBaby)
+app.get('/baby/female', getFemaleBaby)
+
+app.post('/condition/create/:id', requireBaby, createBabyCondition)
+app.delete('/condition/delete/:id', requireBaby, DeleteBabyCondition)
+app.get('/condition/:id', requireBaby, getBabyConditions)
+app.get('/condition/detail/:id', requireBaby, getDetailBabyCondition)
+app.put('/condition/update/:id', requireBaby, updateBabyCondition)
 
 app.listen(port, () => {
   logger.info(`listening on http://localhost:${port}`)
