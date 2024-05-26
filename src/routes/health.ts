e1ed59c3cbdb6e732e65a988e94ccc687ba65282
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-05-26 09:58:19 +0700

    add base router

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -5,5 +5,5 @@ export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Health check succes')
-  res.status(200).send({ status: '200' })
+  res.status(200).send({ status: '200', message: 'Bayi Sehat API is Ready' })
 })
