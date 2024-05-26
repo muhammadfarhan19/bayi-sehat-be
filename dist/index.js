38fc720dd033f987728a1fa583b9b7a7a8616f75
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-05-26 21:38:45 +0700

    fix

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ const logger_1 = require("./utils/logger");
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const deserializedToken_1 = __importDefault(require("./middleware/deserializedToken"));
-const routes_1 = __importDefault(require("./routes"));
+const index_copy_1 = require("./routes/index copy");
 const app = (0, express_1.default)();
 const port = 4000;
 const allowedOrigins = ['http://localhost:3000', 'https://bayi-sehat.vercel.app'];
@@ -37,7 +37,8 @@ app.use(deserializedToken_1.default);
 // app.get('/', (req, res) => {
 //   res.send('Express on Vercel')
 // })
-app.use('/api', routes_1.default);
+// app.use('/api', router)
+(0, index_copy_1.routes)(app);
 app.listen(port, () => {
     logger_1.logger.info(`listening on http://localhost:${port}`);
 });
