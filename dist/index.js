0b4a8f5a295d8291a3fbefff2e906c6ed1a7576f
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-05-26 15:52:18 +0700

    build

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,6 @@ const logger_1 = require("./utils/logger");
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const deserializedToken_1 = __importDefault(require("./middleware/deserializedToken"));
-const auth_controller_1 = require("./controllers/auth.controller");
-const baby_controller_1 = require("./controllers/baby.controller");
-const baby_1 = require("./middleware/baby");
-const condition_controller_1 = require("./controllers/condition.controller");
 const app = (0, express_1.default)();
 const port = 4000;
 const allowedOrigins = ['http://localhost:3000', 'https://bayi-sehat.vercel.app'];
@@ -40,22 +36,22 @@ app.use(deserializedToken_1.default);
 app.get('/', (req, res) => {
     res.send('Bayi Sehat API is Ready to Use');
 });
-app.get('/auth/', auth_controller_1.getUsers);
-app.post('/auth/register', auth_controller_1.userRegistration);
-app.post('/auth/login', auth_controller_1.createUserSession);
-app.post('/auth/refresh', auth_controller_1.refreshSession);
-app.get('/baby/', baby_controller_1.getBaby);
-app.get('/baby/detail/:id', baby_controller_1.getBabyDetail);
-app.post('/baby/create', baby_controller_1.createBaby);
-app.delete('/baby/delete/:id', baby_controller_1.deleteBaby);
-app.put('/baby/put/:id', baby_controller_1.updateBaby);
-app.get('/baby/male', baby_controller_1.getMaleBaby);
-app.get('/baby/female', baby_controller_1.getFemaleBaby);
-app.post('/condition/create/:id', baby_1.requireBaby, condition_controller_1.createBabyCondition);
-app.delete('/condition/delete/:id', baby_1.requireBaby, condition_controller_1.DeleteBabyCondition);
-app.get('/condition/:id', baby_1.requireBaby, condition_controller_1.getBabyConditions);
-app.get('/condition/detail/:id', baby_1.requireBaby, condition_controller_1.getDetailBabyCondition);
-app.put('/condition/update/:id', baby_1.requireBaby, condition_controller_1.updateBabyCondition);
+// app.get('/auth/', getUsers)
+// app.post('/auth/register', userRegistration)
+// app.post('/auth/login', createUserSession)
+// app.post('/auth/refresh', refreshSession)
+// app.get('/baby/', getBaby)
+// app.get('/baby/detail/:id', getBabyDetail)
+// app.post('/baby/create', createBaby)
+// app.delete('/baby/delete/:id', deleteBaby)
+// app.put('/baby/put/:id', updateBaby)
+// app.get('/baby/male', getMaleBaby)
+// app.get('/baby/female', getFemaleBaby)
+// app.post('/condition/create/:id', requireBaby, createBabyCondition)
+// app.delete('/condition/delete/:id', requireBaby, DeleteBabyCondition)
+// app.get('/condition/:id', requireBaby, getBabyConditions)
+// app.get('/condition/detail/:id', requireBaby, getDetailBabyCondition)
+// app.put('/condition/update/:id', requireBaby, updateBabyCondition)
 app.listen(port, () => {
     logger_1.logger.info(`listening on http://localhost:${port}`);
 });
